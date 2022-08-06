cb413d054cf1c9baf4405d841279d08e36bd43e9
miss-f-g <fatemeghafari98@gmail.com>
2022-08-06 11:01:43 +0430

    4.write playSong & pauseSong funcs

--- app.js
+++ app.js
@@ -25,3 +25,31 @@ function loadSong(song) {
   audio.src = `music/${song}.mp3`;
   cover.src = `img/${song}.PNG`;
 }
+
+
+function playSong() {
+  musicContainer.classList.add("play");
+  playBtn.querySelector("i.fas").classList.remove("fa-play");
+  playBtn.querySelector("i.fas").classList.add("fa-pause");
+
+  audio.play();
+}
+
+function pauseSong() {
+  musicContainer.classList.remove("play");
+  playBtn.querySelector("i.fas").classList.add("fa-play");
+  playBtn.querySelector("i.fas").classList.remove("fa-pause");
+
+  audio.pause();
+}
+
+// Event listeners
+playBtn.addEventListener("click", () => {
+  const isPlaying = musicContainer.classList.contains("play");
+
+  if (isPlaying) {
+    pauseSong();
+  } else {
+    playSong();
+  }
+});
