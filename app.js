8b0e44eaf644e1956e8cb271320cfca5b091f333
miss-f-g <fatemeghafari98@gmail.com>
2022-08-06 11:13:54 +0430

    6.add updateProgress func

--- app.js
+++ app.js
@@ -64,6 +64,12 @@ function nextSong() {
   playSong();
 }
 
+function updateProgress(e) {
+  const { duration, currentTime } = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+}
+
 // Event listeners
 playBtn.addEventListener("click", () => {
   const isPlaying = musicContainer.classList.contains("play");
@@ -77,3 +83,4 @@ playBtn.addEventListener("click", () => {
 
 prevBtn.addEventListener("click", prevSong);
 nextBtn.addEventListener("click", nextSong);
+audio.addEventListener("timeupdate", updateProgress);
